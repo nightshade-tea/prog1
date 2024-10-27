cad58b954f81c20726fcd71f5275503046854d00
pedro friedrich <phfr24@inf.ufpr.br>
2024-10-27 20:47:00 -0300

    add lista_retira()

--- tp4/lista.c
+++ tp4/lista.c
@@ -5,9 +5,11 @@
 //
 // Implementação com lista encadeada dupla não-circular
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "lista.h"
 
-/* Cria um item com valor, ant e prox especificados nos parametros.
+/* Cria um item com valor, ant e prox especificados nos parâmetros.
  * Retorno: ponteiro p/ o novo item ou NULL em caso de erro. */
 struct item_t *item_cria(int valor, struct item_t *ant, struct item_t *prox)
 {
@@ -73,7 +75,7 @@ int lista_insere(struct lista_t *lst, int item, int pos)
     if (novo == NULL)
         return -1;
 
-    // inserir no inicio
+    // inserir no início
     if (pos == 0) {
         novo->prox = lst->prim;
         lst->prim = novo;
@@ -85,7 +87,7 @@ int lista_insere(struct lista_t *lst, int item, int pos)
     }
 
     // inserir no fim
-    if (pos == -1 || pos >= tamanho) {
+    if (pos == -1 || pos >= lst->tamanho) {
         novo->ant = lst->ult;
         lst->ult = novo;
 
@@ -95,14 +97,15 @@ int lista_insere(struct lista_t *lst, int item, int pos)
         return ++(lst->tamanho);
     }
 
+    // caso geral
     if (pos < (lst->tamanho / 2)) {
         novo->ant = lst->prim;
 
         int i;
-        for (i = 0; i < pos - 1; i++) // prim..pos-1
+        for (i = 0; i < pos - 1; i++)
             novo->ant = novo->ant->prox;
 
-        // apos o for, novo->ant aponta para lst[pos-1]
+        // após o for, novo->ant aponta para lst[pos-1]
 
         novo->prox = novo->ant->prox;
         novo->ant->prox = novo;
@@ -111,10 +114,10 @@ int lista_insere(struct lista_t *lst, int item, int pos)
         novo->prox = lst->ult;
 
         int i;
-        for (i = lst->tamanho - 1; i > pos; i--) // ult..pos
+        for (i = lst->tamanho - 1; i > pos; i--)
             novo->prox = novo->prox->ant;
 
-        // apos o for, novo->prox aponta para lst[pos]
+        // após o for, novo->prox aponta para lst[pos]
 
         novo->ant = novo->prox->ant;
         novo->ant->prox = novo;
@@ -122,4 +125,75 @@ int lista_insere(struct lista_t *lst, int item, int pos)
     }
 
     return ++(lst->tamanho);
+}
+
+int lista_retira(struct lista_t *lst, int *item, int pos)
+{
+    if (lst == NULL || item == NULL)
+        return -1;
+
+    if (lst->tamanho == 0 || pos < -1)
+        return -1;
+
+    struct item_t *aux;
+
+    // retirar do início
+    if (pos == 0) {
+        aux = lst->prim;
+
+        *item = aux->valor;
+        lst->prim = aux->prox;
+
+        if (aux->prox != NULL)
+            aux->prox->ant = NULL;
+
+        free(aux);
+
+        return --(lst->tamanho);
+    }
+
+    // retirar do fim
+    if (pos == -1 || pos >= lst->tamanho) {
+        aux = lst->ult;
+
+        *item = aux->valor;
+        lst->ult = aux->ant;
+
+        if (aux->ant != NULL)
+            aux->ant->prox = NULL;
+
+        free(aux);
+
+        return --(lst->tamanho);
+    }
+
+    // caso geral
+    if (pos < (lst->tamanho / 2)) {
+        aux = lst->prim;
+
+        int i;
+        for (i = 0; i < pos; i++) // prim..pos
+            aux = aux->prox;
+
+        // apos o for, aux aponta para lst[pos]
+
+        aux->ant->prox = aux->prox;
+        aux->prox->ant = aux->ant;
+    } else {
+        aux = lst->ult;
+
+        int i;
+        for (i = lst->tamanho - 1; i > pos; i--) // ult..pos
+            aux = aux->ant;
+
+        // apos o for, aux aponta para lst[pos]
+
+        aux->ant->prox = aux->prox;
+        aux->prox->ant = aux->ant;
+    }
+
+    *item = aux->valor;
+    free(aux);
+
+    return --(lst->tamanho);
 }
