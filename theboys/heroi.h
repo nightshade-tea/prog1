dfac7cad53d37f6301d028f1d53553253d604574
phfr24 <phfr24@inf.ufpr.br>
2024-11-22 12:17:34 -0300

    add heroi.h

---
+++ theboys/heroi.h
@@ -0,0 +1,30 @@
+#ifndef HEROI
+#define HEROI
+
+#include "conjunto.h"
+
+#define H_MIN_PACIENCIA 0
+#define H_MAX_PACIENCIA 100
+
+#define H_MIN_VELOCIDADE 50
+#define H_MAX_VELOCIDADE 5000
+
+#define H_MIN_HABILIDADES 1
+#define H_MAX_HABILIDADES 3
+
+#define H_EXPERIENCIA_INICIAL 0
+
+struct heroi_t {
+    int id;
+    struct cjto_t *habilidades;
+    int paciencia;
+    int velocidade;
+    int experiencia;
+    int base;
+};
+
+struct heroi_t *heroi_cria(int id, int base);
+
+void heroi_destroi(struct heroi_t **heroi);
+
+#endif
