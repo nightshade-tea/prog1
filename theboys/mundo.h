9bdb6778935b75ffbdddbc203ac1297c8862e8fa
phfr24 <phfr24@inf.ufpr.br>
2024-11-23 15:19:13 -0300

    add mundo.h

---
+++ theboys/mundo.h
@@ -0,0 +1,28 @@
+#ifndef MUNDO
+#define MUNDO
+
+#include "missao.h"
+#include "heroi.h"
+#include "base.h"
+
+#define T_INICIO 0
+#define T_FIM_DO_MUNDO 525600
+
+#define N_TAMANHO_MUNDO 20000
+#define N_HABILIDADES 10
+#define N_HEROIS (N_HABILIDADES * 5)
+#define N_BASES (N_HEROIS / 5)
+#define N_MISSOES (T_FIM_DO_MUNDO / 100)
+
+struct mundo_t {
+    struct **heroi_t herois;
+    struct **base_t bases;
+    struct **missao_t missoes;
+    int relogio;
+};
+
+struct mundo_t *mundo_cria();
+
+void mundo_destroi(struct mundo_t **mundo);
+
+#endif
