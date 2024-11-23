34204bd56017bbba1cbb61fe9d081460c9b24cae
phfr24 <phfr24@inf.ufpr.br>
2024-11-23 15:50:58 -0300

    add fila.h

---
+++ theboys/fila.h
@@ -0,0 +1,25 @@
+#ifndef FILA
+#define FILA
+
+struct fnodo_t {
+    void *item;
+    struct fnodo_t *prox;
+};
+
+struct fila_t {
+    struct fnodo_t *prim;
+    struct fnodo_t *ult;
+    int num;
+};
+
+struct fila_t *fila_cria();
+
+void fila_destroi(struct fila_t **fila);
+
+int fila_insere(struct fila_t *fila, void *item);
+
+void *fila_retira(struct fila_t *fila);
+
+int fila_tamanho(struct fila_t *fila);
+
+#endif
