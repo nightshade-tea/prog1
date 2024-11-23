b26b9000a2b6d0a3999a219b7555cf8d4958c0a0
phfr24 <phfr24@inf.ufpr.br>
2024-11-23 14:35:13 -0300

    add missao.h

---
+++ theboys/missao.h
@@ -0,0 +1,23 @@
+#ifndef MISSAO
+#define MISSAO
+
+#include "conjunto.h"
+
+#define M_MIN_HABILIDADES 6
+#define M_MAX_HABILIDADES 10
+
+#define M_MIN_PERIGO 0
+#define M_MAX_PERIGO 100
+
+struct missao_t {
+    int id;
+    struct cjto_t *habilidades;
+    int perigo;
+    // ponto (local)
+};
+
+struct missao_t *missao_cria(int id);
+
+void missao_destroi(struct missao_t **missao);
+
+#endif
