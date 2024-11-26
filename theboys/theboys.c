6b31a871b3733177996648cc7db9f80feb80ee6c
phfr24 <phfr24@inf.ufpr.br>
2024-11-26 18:00:45 -0300

    update theboys.c

--- theboys/theboys.c
+++ theboys/theboys.c
@@ -1,32 +1,157 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "eventos.h"
+#include "common.h"
 #include "missao.h"
 #include "mundo.h"
 #include "heroi.h"
 #include "base.h"
 #include "fila.h"
 
-#define EV_CHEGA 0
-#define EV_ESPERA 1
-#define EV_DESISTE 2
-#define EV_AVISA 3
-#define EV_ENTRA 4
-#define EV_SAI 5
-#define EV_VIAJA 6
-#define EV_MORRE 7
-#define EV_MISSAO 8
-#define EV_FIM 9
+struct params_t {
+    struct heroi_t *h;
+    struct base_t *b;
+    struct missao_t *m;
+};
 
-void chega(struct heroi_t *h, struct base_t *b);
-void espera(struct heroi_t *h, struct base_t *b);
-void desiste(struct heroi_t *h, struct base_t *b);
-void avisa(struct base_t *b);
-void entra(struct heroi_t *h, struct base_t *b);
-void sai(struct heroi_t *h, struct base_t *b);
-void viaja(struct heroi_t *h, struct base_t *b);
-void morre(struct heroi_t *h, struct base_t *b);
-void missao(struct missao_t *m);
-void fim();
+struct params_t *params_cria(struct heroi_t *h, struct base_t *b,
+                             struct missao_t *m)
+{
+    struct params_t *p = malloc(sizeof(struct params_t));
+
+    if (p == NULL)
+        return NULL;
+
+    p->h = h;
+    p->b = b;
+    p->m = m;
+
+    return p;
+}
+
+void params_destroi(struct params_t **p)
+{
+    if (p == NULL || *p == NULL)
+        return;
+
+    free(*p);
+    *p = NULL;
+}
+
+int agendar_chegadas(struct mundo_t *w, struct fprio_t *lef)
+{
+    if (lef == NULL || w == NULL || w->herois == NULL || w->bases == NULL)
+        return -1;
+
+    int i;
+    for (i = 0; i < N_HEROIS; i++) {
+        int b = aleat(0, N_BASES - 1);
+        int t = aleat(0, 4320); // 4320 = 3 dias
+
+        struct params_t *p = params_cria(w->herois[i], w->bases[b], NULL);
+
+        if (p == NULL || fprio_insere(lef, p, EV_CHEGA, t) == -1)
+            return -1;
+    }
+
+    return 0;
+}
+
+int agendar_missoes(struct mundo_t *w, struct fprio_t *lef)
+{
+    if (lef == NULL || w == NULL || w->missoes == NULL)
+        return -1;
+
+    int i;
+    for (i = 0; i < N_MISSOES; i++) {
+        int t = aleat(0, T_FIM_DO_MUNDO);
+
+        struct params_t *p = params_cria(NULL, NULL, w->missoes[i]);
+
+        if (p == NULL || fprio_insere(lef, p, EV_MISSAO, t) == -1)
+            return -1;
+    }
+
+    return 0;
+}
+
+int agendar_fim(struct fprio_t *lef)
+{
+    if (lef == NULL)
+        return -1;
+
+    struct params_t *p = params_cria(NULL, NULL, NULL);
+
+    if (p == NULL || fprio_insere(lef, p, EV_FIM, T_FIM_DO_MUNDO) == -1)
+        return -1;
+
+    return 0;
+}
 
 int main()
 {
+    srand(0);
+
+    struct mundo_t *w = mundo_cria();
+
+    if (w == NULL)
+        erro("FALHA NA ALOCAÇÃO EM mundo_cria()");
+
+    struct fprio_t *lef = fprio_cria();
+
+    if (lef == NULL)
+        erro("FALHA NA ALOCAÇÃO EM fprio_cria()");
+
+    if (agendar_chegadas(w, lef) == -1)
+        erro("ERRO EM agendar_chegadas()");
+
+    if (agendar_missoes(w, lef) == -1)
+        erro("ERRO EM agendar_missoes()");
+
+    if (agendar_fim(lef) == -1)
+        erro("ERRO EM agendar_fim()");
+
+    int ev;
+    do {
+        struct params_t *p = fprio_retira(lef, &ev, &(w->clk));
+
+        if (p == NULL)
+            erro("FALHA AO RETIRAR EVENTO DA LEF");
+
+        switch (ev) {
+        case EV_CHEGA:
+            chega(lef, p->h, p->b);
+            break;
+        case EV_ESPERA:
+            espera(lef, p->h, p->b);
+            break;
+        case EV_DESISTE:
+            desiste(lef, p->h, p->b);
+            break;
+        case EV_AVISA:
+            avisa(lef, p->b);
+            break;
+        case EV_ENTRA:
+            entra(lef, p->h, p->b);
+            break;
+        case EV_SAI:
+            sai(lef, p->h, p->b);
+            break;
+        case EV_VIAJA:
+            viaja(lef, p->h, p->b);
+            break;
+        case EV_MORRE:
+            morre(lef, p->h, p->b);
+            break;
+        case EV_MISSAO:
+            missao(lef, p->m);
+            break;
+        default:
+            erro("EVENTO INVALIDO");
+        }
+    } while (ev != EV_FIM);
+
+    fim();
+
     return 0;
 }
