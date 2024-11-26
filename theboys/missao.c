a3ca846993cfccc0bee492d4832771ea5e46516d
phfr24 <phfr24@inf.ufpr.br>
2024-11-25 21:04:35 -0300

    add missao.c

---
+++ theboys/missao.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "conjunto.h"
+#include "common.h"
+#include "missao.h"
+#include "ponto.h"
+#include "mundo.h"
+
+void missao_destroi(struct missao_t **m)
+{
+    if (m == NULL || *m == NULL)
+        return;
+
+    (*m)->habs = cjto_destroi((*m)->habs);
+    ponto_destroi(&((*m)->loc));
+
+    free(*m);
+    *m = NULL;
+}
+
+struct missao_t *missao_cria(int id)
+{
+    struct missao_t *m = malloc(sizeof(struct missao_t));
+
+    if (m == NULL)
+        return NULL;
+
+    m->loc = ponto_cria(aleat(0, N_TAMANHO_MUNDO - 1),
+                        aleat(0, N_TAMANHO_MUNDO - 1));
+
+    m->habs = cjto_cria(aleat(M_MIN_HABILIDADES, M_MAX_HABILIDADES));
+    m->per = aleat(M_MIN_PERIGO, M_MAX_PERIGO);
+    m->id = id;
+
+    if (m->loc == NULL || m->habs == NULL) {
+        missao_destroi(&m);
+        return NULL;
+    }
+
+    return m;
+}
