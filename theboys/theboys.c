bfcb5537c346334277c438fbf7fdb9aa53b9b895
phfr24 <phfr24@inf.ufpr.br>
2024-11-26 11:58:23 -0300

    update theboys.c

--- theboys/theboys.c
+++ theboys/theboys.c
@@ -1,21 +1,32 @@
-// programa principal do projeto "The Boys - 2024/2"
-// Autor: xxxxx, GRR xxxxxx
+#include "missao.h"
+#include "mundo.h"
+#include "heroi.h"
+#include "base.h"
+#include "fila.h"
 
-// seus #includes vão aqui
+#define EV_CHEGA 0
+#define EV_ESPERA 1
+#define EV_DESISTE 2
+#define EV_AVISA 3
+#define EV_ENTRA 4
+#define EV_SAI 5
+#define EV_VIAJA 6
+#define EV_MORRE 7
+#define EV_MISSAO 8
+#define EV_FIM 9
 
-// seus #defines vão aqui
+void chega(struct heroi_t *h, struct base_t *b);
+void espera(struct heroi_t *h, struct base_t *b);
+void desiste(struct heroi_t *h, struct base_t *b);
+void avisa(struct base_t *b);
+void entra(struct heroi_t *h, struct base_t *b);
+void sai(struct heroi_t *h, struct base_t *b);
+void viaja(struct heroi_t *h, struct base_t *b);
+void morre(struct heroi_t *h, struct base_t *b);
+void missao(struct missao_t *m);
+void fim();
 
-// minimize o uso de variáveis globais
-
-// programa principal
-int main ()
+int main()
 {
-  // iniciar o mundo
-
-  // executar o laço de simulação
-
-  // destruir o mundo
-
-  return (0) ;
+    return 0;
 }
-
