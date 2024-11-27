9d682a63d4e44a4f467c45ed662b0f19c8039658
phfr24 <phfr24@inf.ufpr.br>
2024-11-27 14:36:05 -0300

    update eventos.c: add espera() and remaining function prototypes

--- theboys/eventos.c
+++ theboys/eventos.c
@@ -31,3 +31,50 @@ void chega(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
     else
         fprio_insere(lef, p, EV_DESISTE, t);
 }
+
+void espera(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+    if (h == NULL || b == NULL || lef == NULL)
+        return;
+
+    fila_insere(b->esp, h);
+
+    struct params_t *p = params_cria(NULL, b, NULL);
+
+    if (p == NULL)
+        return;
+
+    fprio_insere(lef, p, EV_AVISA, t);
+}
+
+void desiste(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void avisa(int t, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void entra(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void sai(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void viaja(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void morre(int t, struct heroi_t *h, struct base_t *b, struct fprio_t *lef)
+{
+}
+
+void missao(int t, struct missao_t *m, struct fprio_t *lef)
+{
+}
+
+void fim(int t)
+{
+}
