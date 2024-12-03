fcba25f6d3291afd12666965ac1fdd38e301aa46
phfr24 <phfr24@inf.ufpr.br>
2024-12-03 16:17:32 -0300

    add comments for ponto.h

--- theboys/ponto.h
+++ theboys/ponto.h
@@ -2,14 +2,19 @@
 #define PONTO
 
 struct ponto_t {
-    int x;
-    int y;
+    int x;  // coordenada x do ponto
+    int y;  // coordenada y do ponto
 };
 
+/* Cria um ponto com as coordenadas especificadas.
+ * Retorno: endereço do novo ponto ou NULL em caso de erro. */
 struct ponto_t *ponto_cria(int x, int y);
 
+/* Destroi um ponto e aterra seu ponteiro. */
 void ponto_destroi(struct ponto_t **p);
 
+/* Calcula a distância cartesiana entre dois pontos.
+ * Retorno: distância entre os pontos ou -1 em caso de erro. */
 int ponto_distancia(struct ponto_t *p, struct ponto_t *q);
 
 #endif
