dbf76c38e7bbad4c60ad32c27eaf3b27bf4e8614
phfr24 <phfr24@inf.ufpr.br>
2024-12-03 13:41:56 -0300

    add comments for common.h

--- theboys/common.h
+++ theboys/common.h
@@ -1,7 +1,10 @@
 #ifndef COMMON
 #define COMMON
 
+/* Gera um número aleatório entre min e max, inclusive. */
 int aleat(int min, int max);
+
+/* Imprime a mensagem em STDERR. */
 void erro(const char *msg);
 
 #endif
